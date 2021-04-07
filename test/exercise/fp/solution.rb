f2a3c84f04482efb535d8b3fa2b35d17082a90d8
Impulse73 <doter133779@gmail.com>
2021-04-07 23:48:52 +0400

    edit soluton of exercise fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,52 +5,18 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        subarr = []
-        array.map { |value|
-          if value['country'].nil?
-            0
-          else
-            str = value['country']
-            if str.include?','
-              subarr.append(value)
-            else
-              0
-            end
-          end
+        selected = array.select { |film|
+          film['country'].to_s.split(',').length > 1 && film['rating_kinopoisk'].to_f.positive?
         }
-        subarr.map! { |value| value['rating_kinopoisk'].nil? ? 0 : value }
-        subarr.delete(0)
-        subarr.map! { |value|
-          str = value['rating_kinopoisk']
-          if (str.length == 1) && (str == '0')
-            0
-          else
-            value
-          end
-        }
-        subarr.delete(0)
-        summ =  subarr.reduce(0) { |sum, n|
-          str = n['rating_kinopoisk']
-          sum += str.to_f
-        }
-        summ / subarr.length
+        subarray = selected.map { |film| film['rating_kinopoisk'].to_f }
+        summary = subarray.reduce(:+)
+        summary / subarray.length
       end
 
       def chars_count(films, threshold)
-        subarr = []
-        films.map { |value|
-          if value['rating_kinopoisk'].nil?
-            0
-          end
-          rating = value['rating_kinopoisk'].to_f
-          if rating >= threshold
-            subarr.append(value)
-          end
-        }
-        subarr.reduce(0) { |sum, n|
-          str = n['name']
-          sum += str.scan('и').size
-        }
+        selected = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+        films_arr = selected.map { |value| value['name'].to_s }
+        films_arr.reduce(0) { |sum, n| sum + n.count('и') }
       end
     end
   end
